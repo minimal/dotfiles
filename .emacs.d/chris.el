824c3c2c68bad9ba85597e4e1fe7899d90c4b4ba
Chris McDevitt <christophermcdevitt@gmail.com>
2012-04-16 19:16:18 +0100

    emacs: re-enable dedicated window

--- .emacs.d/chris.el
+++ .emacs.d/chris.el
@@ -91,23 +91,23 @@
 
 
 ;;;;; Dedicated window
-;; (defadvice pop-to-buffer (before cancel-other-window first)
-;;   (ad-set-arg 1 nil))
+(defadvice pop-to-buffer (before cancel-other-window first)
+  (ad-set-arg 1 nil))
 
-;; (ad-activate 'pop-to-buffer)
+(ad-activate 'pop-to-buffer)
 
-;; ;; Toggle window dedication
-;; (defun toggle-window-dedicated ()
-;;   "Toggle whether the current active window is dedicated or not"
-;;   (interactive)
-;;   (message
-;;    (if (let (window (get-buffer-window (current-buffer)))
-;;          (set-window-dedicated-p window 
-;;                                  (not (window-dedicated-p window))))
-;;        "Window '%s' is dedicated"
-;;      "Window '%s' is normal")
-;;    (current-buffer)))
+;; Toggle window dedication
+(defun toggle-window-dedicated ()
+  "Toggle whether the current active window is dedicated or not"
+  (interactive)
+  (message
+   (if (let (window (get-buffer-window (current-buffer)))
+         (set-window-dedicated-p window 
+                                 (not (window-dedicated-p window))))
+       "Window '%s' is dedicated"
+     "Window '%s' is normal")
+   (current-buffer)))
 
-;; ;; Press [pause] key in each window you want to "freeze"
-;; (global-set-key [pause] 'toggle-window-dedicated)
+;; Press [pause] key in each window you want to "freeze"
+(global-set-key [pause] 'toggle-window-dedicated)
 ;;;;;;;;;;;;; dedicated
