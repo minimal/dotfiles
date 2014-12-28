b366b7d62ad76d6aa8ab015eb3550ad5a2595a0d
Chris McDevitt <christophermcdevitt@gmail.com>
2014-12-28 16:45:53 +0000

    replace ido vertical customisations with ido-vertical-mode

--- .emacs.d/chris.el
+++ .emacs.d/chris.el
@@ -71,6 +71,10 @@
 ;; as you can get away with on the :config side. Config doesn't run
 ;; till the mode is activated
 
+(use-package ido-vertical-mode
+  :ensure t
+  :init (ido-vertical-mode 1))
+
 ;; helm
 
 ;; more options http://pages.sachachua.com/.emacs.d/Sacha.html#unnumbered-14
@@ -536,28 +540,6 @@ sticky."
 (ad-activate 'yank-pop)
 
 (add-hook #'prog-mode-hook #'rainbow-delimiters-mode)
-
-;; Ido-mode customizations
-(setq ido-decorations                                                      ; Make ido-mode display vertically
-      (quote
-       ("\n-> "           ; Opening bracket around prospect list
-        ""                ; Closing bracket around prospect list
-        "\n   "           ; separator between prospects
-        "\n   ..."        ; appears at end of truncated list of prospects
-        "["               ; opening bracket around common match string
-        "]"               ; closing bracket around common match string
-        " [No match]"     ; displayed when there is no match
-        " [Matched]"      ; displayed if there is a single match
-        " [Not readable]" ; current diretory is not readable
-        " [Too big]"      ; directory too big
-        " [Confirm]")))   ; confirm creation of new file or buffer
-
-(add-hook 'ido-setup-hook                                                  ; Navigate ido-mode vertically
-          (lambda ()
-            (define-key ido-completion-map [down] 'ido-next-match)
-            (define-key ido-completion-map [up] 'ido-prev-match)
-            (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
-            (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)))
 
 ;; go
 (add-hook 'go-mode-hook 'flycheck-mode)
