c1d71ca79fd6eebedcc0c0ae0f80803374602a4a
Chris McDevitt <christophermcdevitt@gmail.com>
2015-05-07 17:39:13 +0900

    more uncommitted stuff from before

--- .emacs.d/chris.el
+++ .emacs.d/chris.el
@@ -319,7 +319,8 @@ sticky."
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
+;; run M-x haskell-process-load-or-reload
+;; M-? to show error at cursor
 
 ;; end haskell
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -404,3 +405,31 @@ If inside a code-block, simply calls `self-insert-command'."
           "~/Dropbox/code/projects.org")
     (add-to-list 'org-capture-templates (org-projectile:project-todo-entry "p")))
   :ensure t)
+;; http://www.emacswiki.org/emacs/TransposeWindows
+(defun rotate-windows (arg)
+  "Rotate your windows; use the prefix argument to rotate the other direction"
+  (interactive "P")
+  (if (not (> (count-windows) 1))
+      (message "You can't rotate a single window!")
+    (let* ((rotate-times (if (and (numberp arg) (not (= arg 0))) arg 1))
+           (direction (if (or (< rotate-times 0) (equal arg '(4)))
+                          'reverse
+                        (lambda (x) x)))
+           (i 0))
+      (while (not (= rotate-times 0))
+        (while  (< i (- (count-windows) 1))
+          (let* ((w1 (elt (funcall direction (window-list)) i))
+                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
+                 (b1 (window-buffer w1))
+                 (b2 (window-buffer w2))
+                 (s1 (window-start w1))
+                 (s2 (window-start w2))
+                 (p1 (window-point w1))
+                 (p2 (window-point w2)))
+            (set-window-buffer-start-and-point w1 b2 s2 p2)
+            (set-window-buffer-start-and-point w2 b1 s1 p1)
+            (setq i (1+ i))))
+
+        (setq i 0
+              rotate-times
+              (if (< rotate-times 0) (1+ rotate-times) (1- rotate-times)))))))
