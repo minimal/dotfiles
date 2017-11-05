e4e8ff9a75243d8f0eaef8ab4caabe2abea263ed
Christopher McDevitt <christophermcdevitt@gmail.com>
2017-11-05 20:08:25 +0000

    tweaks

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -1,10 +1,2 @@
 {
-  packageOverrides = super: let self = super.pkgs; in
-  {
-    haskellEnv = self.haskellngPackages.ghcWithPackages (p: with p; [
-      cabal2nix
-      cabal-install
-      hlint
-    ]);
-  };
 }
