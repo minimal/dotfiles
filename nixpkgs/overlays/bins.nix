b4560cb450ef442400a476969fb4cf3b3ce4f257
Chris McDevitt <christophermcdevitt@gmail.com>
2021-06-19 20:24:31 +0100

    nix: install babashka bin in linux as well
    choose source based on platform

--- nixpkgs/overlays/bins.nix
+++ nixpkgs/overlays/bins.nix
@@ -78,29 +78,40 @@ self: super: {
     };
   };
 
-  babashka-bin = with super; stdenv.mkDerivation rec {
-    name = "babashka-${version}";
-    version = "0.4.6";
+  babashka-bin =
+    with super; stdenv.mkDerivation rec {
+      name = "babashka-${version}";
+      version = "0.4.6";
+      src =
+        let
+          sources = {
+            "x86_64-darwin" = fetchzip {
+              url = "https://github.com/babashka/babashka/releases/download/v0.4.6/babashka-0.4.6-macos-amd64.tar.gz";
+              sha256 = "075v0vgnifjz4073r5hvky33bqm3acycvmqbi1lfbbph2glsi1nc";
+              name = "babashka";
+            };
+            "x86_64-linux" = fetchzip {
+              url = "https://github.com/babashka/babashka/releases/download/v0.4.6/babashka-0.4.6-linux-amd64.tar.gz";
+              sha256 = "12wif13q43hk441j35nbb9nn4k26x40lhgqkc1mamwcnl30bilaj";
+              name = "babashka";
+            };
+          };
+        in
+        sources."${stdenv.hostPlatform.system}";
 
-    src = fetchzip {
-      url = "https://github.com/babashka/babashka/releases/download/v0.4.6/babashka-0.4.6-macos-amd64.tar.gz";
-      sha256 = "075v0vgnifjz4073r5hvky33bqm3acycvmqbi1lfbbph2glsi1nc";
-      name = "babashka";
+      phases = [ "installPhase" ];
+
+      installPhase = ''
+        mkdir -p $out/bin
+        cp -p $src/bb $out/bin/bb
+      '';
+
+      meta = with lib; {
+        description = "Native, fast starting Clojure interpreter for scripting";
+        homepage = https://github.com/babashka/babashka;
+        platforms = [ "x86_64-darwin" "x86_64-linux" ];
+      };
     };
-
-    phases = [ "installPhase" ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -p $src/bb $out/bin/bb
-    '';
-
-    meta = with lib; {
-      description = "Native, fast starting Clojure interpreter for scripting";
-      homepage = https://github.com/babashka/babashka;
-      platforms = [ "x86_64-darwin" ];
-    };
-  };
 }
 # See for fetching different archs
 # https://github.com/jtacoma/nixpkgs/blob/42e09c2134add3ae66c6579478c474aeffd8443d/pkgs/development/interpreters/dart/default.nix
