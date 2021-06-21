83f144fde3a3416179951fab0e29242b66821475
Chris McDevitt <christophermcdevitt@gmail.com>
2021-06-21 17:46:23 +0100

    nix(linux): use prezto zsh, git
    Copy mac zsh config to zsh.nix, import in linux.nix
    delete old linux zsh config
    import git.nix

--- nixpkgs/linux.nix
+++ nixpkgs/linux.nix
@@ -2,7 +2,11 @@
 # to enable:
 # ln -s linux.nix home.nix
 {
-  imports = [ ./_home.nix ];
+  imports = [
+    ./_home.nix
+    ./git.nix
+    ./zsh.nix
+  ];
   home.packages = with pkgs; [
     unzip
     trash-cli
@@ -13,95 +17,23 @@
     VISUAL = "nvim";
   };
 
-  programs.zsh = {
-    enable = true;
-    autocd = true;
-    dotDir = ".config/zsh";
-    enableAutosuggestions = true;
-    enableCompletion = true;
-    shellAliases = {
-      sl = "exa";
-      # ls = "exa";
-      l = "exa -l";
-      la = "exa -la";
-      ip = "ip --color=auto";
-      code = "/mnt/c/Users/Chris/AppData/Local/Programs/Microsoft\\ VS\\ Code/bin/code";
+  programs = {
+    git.userEmail = "christophermcdevitt@gmail.com";
+    zsh = {
+      shellAliases = {
+        code = "/mnt/c/Users/Chris/AppData/Local/Programs/Microsoft\\ VS\\ Code/bin/code";
+      };
     };
-    initExtraFirst = '' '';
-    initExtra = ''
-        bindkey '^ ' autosuggest-accept
-        AGKOZAK_CMD_EXEC_TIME=5
-        AGKOZAK_COLORS_CMD_EXEC_TIME='yellow'
-        AGKOZAK_COLORS_PROMPT_CHAR='magenta'
-        AGKOZAK_CUSTOM_SYMBOLS=( '⇣⇡' '⇣' '⇡' '+' 'x' '!' '>' '?' )
-        AGKOZAK_MULTILINE=0
-        AGKOZAK_PROMPT_CHAR=( ❯ ❯ ❮ )
-        #eval $(thefuck --alias)
-        #autopair-init
-      . /home/chris/.nix-profile/etc/profile.d/nix.sh 
-    '';
-    # nix path getting wiped unless done here on wsl ubuntu
-    profileExtra = " ";
-    sessionVariables = rec {
-      NVIM_TUI_ENABLE_TRUE_COLOR = "1";
-      ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE = "fg=3";
-      DEV_ALLOW_ITERM2_INTEGRATION = "1";
 
-      EDITOR = "vim";
-      VISUAL = EDITOR;
-      GIT_EDITOR = EDITOR;
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+      defaultOptions = [
+        # "--height 40%"
+        "--layout=reverse"
+        "--border"
+        "--inline-info"
+      ];
     };
-    plugins = with pkgs; [
-      {
-        name = "agkozak-zsh-prompt";
-        src = fetchFromGitHub {
-          owner = "agkozak";
-          repo = "agkozak-zsh-prompt";
-          rev = "v3.7.0";
-          sha256 = "1iz4l8777i52gfynzpf6yybrmics8g4i3f1xs3rqsr40bb89igrs";
-        };
-        file = "agkozak-zsh-prompt.plugin.zsh";
-      }
-      {
-        name = "formarks";
-        src = fetchFromGitHub {
-          owner = "wfxr";
-          repo = "formarks";
-          rev = "8abce138218a8e6acd3c8ad2dd52550198625944";
-          sha256 = "1wr4ypv2b6a2w9qsia29mb36xf98zjzhp3bq4ix6r3cmra3xij90";
-        };
-        file = "formarks.plugin.zsh";
-      }
-      {
-        name = "zsh-syntax-highlighting";
-        src = fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "0.6.0";
-          sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
-        };
-        file = "zsh-syntax-highlighting.zsh";
-      }
-      {
-        name = "zsh-abbrev-alias";
-        src = fetchFromGitHub {
-          owner = "momo-lab";
-          repo = "zsh-abbrev-alias";
-          rev = "637f0b2dda6d392bf710190ee472a48a20766c07";
-          sha256 = "16saanmwpp634yc8jfdxig0ivm1gvcgpif937gbdxf0csc6vh47k";
-        };
-        file = "abbrev-alias.plugin.zsh";
-      }
-      {
-        name = "zsh-autopair";
-        src = fetchFromGitHub {
-          owner = "hlissner";
-          repo = "zsh-autopair";
-          rev = "34a8bca0c18fcf3ab1561caef9790abffc1d3d49";
-          sha256 = "1h0vm2dgrmb8i2pvsgis3lshc5b0ad846836m62y8h3rdb3zmpy1";
-        };
-        file = "autopair.zsh";
-      }
-    ];
   };
 }
---
+++ nixpkgs/zsh.nix
@@ -0,0 +1,94 @@
+let
+  HOME = builtins.getEnv "HOME";
+in
+{
+  programs.zsh = {
+    enable = true;
+    autocd = true;
+    dotDir = ".config/zsh";
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    history.path = "${HOME}/.zhistory";
+    shellAliases = {
+      sl = "exa";
+      l = "exa";
+      ll = "exa -l";
+      la = "exa -la";
+      ip = "ip --color=auto";
+      gita = "git archive --format=zip `git reflog | grep 'HEAD@{0}' | cut -d \" \" -f1 | sed 's/[.]*//g'` > archive.zip";
+      # gka = "gitk --all&";
+      rm-git-turds = "rm **/(*.orig|*(LOCAL|BASE|REMOTE|BACKUP)*)";
+      mdfindname = "mdfind -name";
+    };
+    prezto = {
+      enable = true;
+      pmodules = [
+        "archive"
+        "autosuggestions"
+        "environment"
+        "terminal"
+        "editor"
+        "history"
+        "directory"
+        "spectrum"
+        # "fasd"
+        "spectrum"
+        "utility"
+        "ssh"
+        "completion"
+        "git"
+        "osx"
+        "syntax-highlighting"
+        "history-substring-search"
+        "node"
+        # "homebrew"
+        # "haskell"
+        "ruby"
+        "prompt"
+      ];
+      prompt = {
+        theme = "powerlevel10k";
+        pwdLength = "long";
+      };
+
+      extraConfig = ''
+        test -e ${HOME}/.iterm2_shell_integration.zsh && source ${HOME}/.iterm2_shell_integration.zsh
+        eval "$(direnv hook zsh)"
+      '';
+    };
+
+
+    profileExtra = ''
+      . ${HOME}/.nix-profile/etc/profile.d/nix.sh
+
+      if [[ -s ${HOME}/.secrets ]]; then
+        source "${HOME}/.secrets"
+      fi
+
+      path=(${HOME}/bin
+            ${HOME}/.local/bin
+            ${HOME}/Downloads/confluent-6.1.1/bin/
+            $path)
+    '';
+    # /usr/local/bin gets added at the front after the above so
+    # overrides some of nix bins etc. How to fix?
+    # + builtins.readFile ../Makefile;
+    initExtra = ''
+      # To customize prompt, run `p10k configure` or edit ~/.config/zsh/.p10k.zsh.
+      [[ ! -f ~/.config/zsh/.p10k.zsh ]] || source ~/.config/zsh/.p10k.zsh
+
+      function grep-port {
+          lsof -n -i4TCP:$1 | grep LISTEN
+      }
+
+      function cdgroot () { cd `git root`; } # relies on a git alias `root = !pwd`
+
+      # awesome!! e.g. $ git <up-arrow> => $ git log
+      bindkey "^[[A" history-beginning-search-backward
+      bindkey "^[[B" history-beginning-search-forward
+      # some keyboards use this:
+      bindkey "^[OA" history-beginning-search-backward
+      bindkey "^[OB" history-beginning-search-forward
+    '';
+  };
+}
