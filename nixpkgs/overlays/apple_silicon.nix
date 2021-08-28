98e7448d3e84ec225466a3cd69722afa2121a4d9
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2021-08-28 16:54:02 +0100

    nix: overlay for apple silicon overrides
    use x86_64-darwin for broken arm packages

---
+++ nixpkgs/overlays/apple_silicon.nix
@@ -0,0 +1,16 @@
+# use x86_64-darwin for packages broken on apple silicon
+final: prev:
+let
+  isM1 = prev.stdenv.hostPlatform.system == "aarch64-darwin";
+  pkgs_x86_64 = import <nixpkgs> { localSystem = "x86_64-darwin"; };
+  pkgs =
+    if isM1
+    then pkgs_x86_64
+    else prev;
+in
+{
+  shellcheck = pkgs.shellcheck;
+  emacsMacport = pkgs.emacsMacport;
+  babashka = pkgs.babashka;
+  ghc = pkgs.ghc;
+}
