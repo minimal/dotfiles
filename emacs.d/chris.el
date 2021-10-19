2e4ea19d391d0c65e9b96baa3c94fb466cfa2a8d
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2021-10-19 20:40:38 +0100

    emacs: delete more

--- emacs.d/chris.el
+++ emacs.d/chris.el
@@ -194,36 +194,6 @@ If inside a code-block, simply calls `self-insert-command'."
 
 ;; (pprint (sort (.split (System/getProperty "java.class.path") ":")))
 
-;; http://www.emacswiki.org/emacs/TransposeWindows
-(defun rotate-windows (arg)
-  "Rotate your windows; use the prefix argument to rotate the other direction"
-  (interactive "P")
-  (if (not (> (count-windows) 1))
-      (message "You can't rotate a single window!")
-    (let* ((rotate-times (if (and (numberp arg) (not (= arg 0))) arg 1))
-           (direction (if (or (< rotate-times 0) (equal arg '(4)))
-                          'reverse
-                        (lambda (x) x)))
-           (i 0))
-      (while (not (= rotate-times 0))
-        (while  (< i (- (count-windows) 1))
-          (let* ((w1 (elt (funcall direction (window-list)) i))
-                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
-                 (b1 (window-buffer w1))
-                 (b2 (window-buffer w2))
-                 (s1 (window-start w1))
-                 (s2 (window-start w2))
-                 (p1 (window-point w1))
-                 (p2 (window-point w2)))
-            (set-window-buffer-start-and-point w1 b2 s2 p2)
-            (set-window-buffer-start-and-point w2 b1 s1 p1)
-            (setq i (1+ i))))
-
-        (setq i 0
-              rotate-times
-              (if (< rotate-times 0) (1+ rotate-times) (1- rotate-times)))))))
-
-
 (defun xah-copy-file-path (&optional φdir-path-only-p)
   "Copy the current buffer's file path or dired path to `kill-ring'.
 Result is full path.
