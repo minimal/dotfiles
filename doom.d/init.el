d32791a88eb2daeac0535cd014e9368223c434ca
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2021-10-19 20:50:46 +0100

    doom: enable docker, gist

--- doom.d/init.el
+++ doom.d/init.el
@@ -87,11 +87,11 @@
        ;;ansible
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        direnv
-       ;;docker
+       docker
        ;;editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       ;;gist              ; interacting with github gists
+       gist              ; interacting with github gists
        lookup              ; navigate your code and its documentation
        ;;lsp               ; M-x vscode
        magit             ; a git porcelain for Emacs
