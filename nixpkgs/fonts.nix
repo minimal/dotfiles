cf1c938ba54a98886611be321c0841b0812e3e2f
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2021-10-26 17:19:30 +0100

    use brew for open source fonts again
    using nix for fonts causes font daemon to peg cpu on mac

--- nixpkgs/fonts.nix
+++ nixpkgs/fonts.nix
@@ -7,22 +7,22 @@
     # jetbrains-mono-nerd-font # not found
     # vollk rn # error
     (pkgs.callPackage ./pkgs/pragmatapro.nix { })
-    alegreya
-    alegreya-sans
-    anonymousPro
-    fira
-    fira-code
-    fira-mono
-    hack-font
-    hasklig
-    inconsolata
-    jetbrains-mono
-    lato
-    monoid
-    open-sans
-    source-code-pro
-    source-sans-pro
-    source-serif-pro
     (pkgs.callPackage ./pkgs/equity.nix { })
+    # alegreya
+    # alegreya-sans
+    # anonymousPro
+    # fira
+    # fira-code
+    # fira-mono
+    # hack-font
+    # hasklig
+    # inconsolata
+    # jetbrains-mono
+    # lato
+    # monoid
+    # open-sans
+    # source-code-pro
+    # source-sans-pro
+    # source-serif-pro
   ];
 }
