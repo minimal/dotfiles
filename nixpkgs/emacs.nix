d27f2fcfa4900c976aecd6d73698ef62b0c4ea41
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2022-03-28 11:15:11 +0100

    nix: emacs sync improvemnt

--- nixpkgs/emacs.nix
+++ nixpkgs/emacs.nix
@@ -11,8 +11,7 @@ let
     if [ $(git -C $HOME/.emacs.d rev-parse HEAD) != ${pkgs.doomEmacsRevision} ]; then
       git -C $HOME/.emacs.d fetch https://github.com/hlissner/doom-emacs.git || true
       git -C $HOME/.emacs.d checkout ${pkgs.doomEmacsRevision} || true
-      $HOME/.emacs.d/bin/doom sync || true
-      YES=1 FORCE=1 $HOME/.emacs.d/bin/doom sync -u &
+      YES=1 FORCE=1 $HOME/.emacs.d/bin/doom sync -u || true
     fi
   '';
   myEmacs = if pkgs.stdenv.isDarwin then pkgs.emacsGcc else pkgs.emacsPgtkGcc;
