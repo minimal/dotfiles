d86355a1cda3c90d92e8c775b5677c062f4113e3
Chris McDevitt <christophermcdevitt@gmail.com>
2025-06-18 11:25:48 +0100

    doom: correctly set pragmata weight to medium
    now doom increase/decrease font size and big font mode work.

--- doom.d/config.el
+++ doom.d/config.el
@@ -84,7 +84,7 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-(setq doom-font (font-spec :family "PragmataPro Liga" :size 13 :weight 'normal))
+(setq doom-font (font-spec :family "PragmataPro Liga" :size 13 :weight 'medium))
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
