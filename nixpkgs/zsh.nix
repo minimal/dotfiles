f19b09a16d249b48437321d7e9ab9c7b74cb6b30
Chris McDevitt <christophermcdevitt@gmail.com>
2025-07-26 17:17:30 +0100

    nix: zsh updates

--- nixpkgs/zsh.nix
+++ nixpkgs/zsh.nix
@@ -83,6 +83,7 @@ in {
             ${HOME}/.cargo/bin
             ${HOME}/.emacs.d/bin
             ${HOME}/.krew/bin
+            ${HOME}/.babashka/bbin/bin
             /opt/homebrew/bin
             /opt/homebrew/sbin
             $path)
@@ -136,6 +137,39 @@ in {
           done
         }
         refresh-just-aliases
+
+    # Initialize the completion system
+    autoload -U compinit && compinit
+      '')
+
+      (lib.mkOrder 800 ''
+        nixify() {
+          if [ ! -e ./.envrc ]; then
+            echo "use nix" > .envrc
+            direnv allow
+          fi
+          if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+            cat > default.nix <<'EOF'
+        with import <nixpkgs> {};
+        mkShell {
+          nativeBuildInputs = [
+            bashInteractive
+          ];
+        }
+        EOF
+            ${config.home.sessionVariables.EDITOR} default.nix
+          fi
+        }
+        flakify() {
+          if [ ! -e flake.nix ]; then
+            nix flake new -t github:nix-community/nix-direnv .
+          elif [ ! -e .envrc ]; then
+            echo "use flake" > .envrc
+            direnv allow
+          fi
+          ${config.home.sessionVariables.EDITOR} flake.nix
+        }
+
       '')
     ];
   };
